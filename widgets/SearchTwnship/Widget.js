7ec62d1590fffd9db1b42c8c63aa147aad780dd2
jaredlang <jaredlang76@gmail.com>
2016-09-07 16:47:00 -0500

    implemented the SearchTownship widget

---
+++ widgets/SearchTwnship/Widget.js
@@ -0,0 +1,277 @@
+
+define([
+		'dojo/_base/declare',
+		'dijit/_WidgetsInTemplateMixin',
+		'jimu/BaseWidget',
+		'dojo/on',
+		'dojo/Deferred',
+		'dojo/dom-construct',
+		'dojo/html',
+		'dojo/_base/lang',
+		'esri/Color',
+		'dojo/_base/array',
+		'dojo/dom-style',
+		'dojo/dom-class',
+		'esri/config',
+		'esri/graphic',
+		'esri/tasks/QueryTask',
+		'esri/tasks/query',
+		'esri/geometry/Extent',
+		'esri/geometry/Point',
+		'esri/geometry/Polyline',
+		'esri/geometry/Polygon',
+		'esri/geometry/webMercatorUtils',
+		'esri/tasks/GeometryService',
+		'esri/layers/GraphicsLayer',
+		'esri/symbols/SimpleMarkerSymbol',
+		'esri/symbols/SimpleLineSymbol',
+		'esri/symbols/SimpleFillSymbol',
+		'esri/InfoTemplate',
+		'esri/layers/FeatureLayer',
+		'jimu/dijit/ViewStack',
+		'jimu/utils',
+		'jimu/SpatialReference/wkidUtils',
+		'jimu/LayerInfos/LayerInfos',
+		"dojo/store/Memory",
+		'jimu/dijit/LoadingIndicator',
+		'jimu/dijit/Popup',
+		'dijit/form/ComboBox'
+	],
+	function (declare, _WidgetsInTemplateMixin, BaseWidget, on, Deferred,
+		domConstruct, html, lang, Color, array, domStyle, domClass,
+		esriConfig, Graphic, QueryTask, Query, Extent, Point, Polyline, Polygon, webMercatorUtils,
+		GeometryService, GraphicsLayer, SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol,
+		InfoTemplate, FeatureLayer, ViewStack, jimuUtils, wkidUtils, LayerInfos,
+		Memory, LoadingIndicator, Popup, ComboBox) {
+
+	var clazz = declare([BaseWidget, _WidgetsInTemplateMixin], {
+			name : 'SearchTwnship',
+			baseClass : 'jimu-widget-searchTwnship',
+			_gs : null,
+			_defaultGsUrl : '//tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer', 
+			_graphicLayer : null,
+			_symbols : {
+				"esriGeometryPolygon" : {
+					"type" : "esriSFS",
+					"style" : "esriSFSSolid",
+					"color" : [255, 0, 0, 64],
+					"outline" : {
+						"type" : "esriSLS",
+						"style" : "esriSLSSolid",
+						"color" : [0, 255, 255, 255],
+						"width" : 2
+					}
+				},
+				"esriGeometryPolyline" : {
+					"type" : "esriSLS",
+					"style" : "esriSLSDashDot",
+					"color" : [0, 255, 255, 255],
+					"width" : 2
+				},
+				"esriGeometryPoint" : {
+					"type" : "esriSMS",
+					"style" : "esriSMSCircle",
+					"color" : [255, 0, 0, 128],
+					"size" : 8,
+					"angle" : 0,
+					"xoffset" : 0,
+					"yoffset" : 0,
+					"outline" : {
+						"type" : "esriSLS",
+						"style" : "esriSLSSolid",
+						"color" : [0, 255, 255, 255],
+						"width" : 1,
+					}
+				}
+			},
+			_vtlDirValue: null,
+			_hrzDirValue: null, 
+			
+			postMixInProperties : function () {
+				this.inherited(arguments);
+
+				if (esriConfig.defaults.geometryService) {
+					this._gs = esriConfig.defaults.geometryService;
+				} else {
+					this._gs = new GeometryService(this._defaultGsUrl);
+				}
+			},
+
+			postCreate : function () {
+				this.inherited(arguments);
+				
+				this._initSearch();
+				this._initSearchForm();
+			},
+
+			_initSearch : function () {
+
+				this._graphicLayer = new GraphicsLayer();
+				this.map.addLayer(this._graphicLayer);
+
+				this._infoTemplate = new InfoTemplate("Properties", "${*}");
+			},
+
+			_initSearchForm : function () {				
+
+			},
+
+			onActive : function () {
+				this.map.setInfoWindowOnClick(false);
+			},
+
+			onDeActive : function () {
+				this.map.setInfoWindowOnClick(true);
+			},
+
+			onClose : function () {
+				this._hideMessage(); 
+				
+				this._graphicLayer.clear();
+				this.map.removeLayer(this._graphicLayer);
+			},
+
+			destroy : function () {},
+
+			startup : function () {
+				this.inherited(arguments);
+
+			},
+			
+			_onVtlDirChanged: function(evt) {
+				var rdoInput = evt.currentTarget; 
+				if (rdoInput.checked) {
+					this._vtlDirValue = rdoInput.value; 
+				}
+			},
+			
+			_onHrzDirChanged: function(evt) {
+				var rdoInput = evt.currentTarget; 
+				if (rdoInput.checked) {
+					this._hrzDirValue = rdoInput.value; 
+				}
+			},
+			
+			_onBtnEndClicked : function () {
+				var whereClause = this.config.state.field + " = '" + this.stateInput.value + "'";
+				if (this.sectionInput.value.trim().length > 0) {
+					whereClause += (" and " + this.config.section.field + " = '" + this.sectionInput.value + "'");
+				}
+				if (this.townshipInput.value.trim().length > 0) {
+					whereClause += (" and " + this.config.township.field + " = '" + this.townshipInput.value + "'");
+				}
+				if (this.rangeInput.value.trim().length > 0) {
+					whereClause += (" and " + this.config.range.field + " = '" + this.rangeInput.value + "'");
+				}
+				if (this._vtlDirValue) {
+					whereClause += (" and " + this.config.vtlDir.field + " = '" + this._vtlDirValue + "'");
+				}
+				if (this._hrzDirValue) {
+					whereClause += (" and " + this.config.hrzDir.field + " = '" + this._hrzDirValue + "'");
+				}
+				
+				this._executeSearch(whereClause);
+			},
+
+			_showMessage : function (textMsg, lvl) {
+				domClass.remove(this.searchMessage);
+				switch (lvl) {
+				case "error":
+					domClass.add(this.searchMessage, "message-error");
+					break;
+				case "warning":
+					domClass.add(this.searchMessage, "message-warning");
+					break;
+				case "info":
+					domClass.add(this.searchMessage, "message-info");
+					break;
+				default:
+					domClass.add(this.searchMessage, "message-info");
+				}
+				this.searchMessage.innerText = textMsg;
+			},
+
+			_hideMessage : function () {
+				this.searchMessage.innerText = "";
+			},
+
+			_executeSearch : function (whereClause) {
+				this._hideMessage(); 
+				
+				var query = new Query();
+				query.where = whereClause;
+				query.outSpatialReference = this.map.spatialReference;
+				query.returnGeometry = true;
+				query.outFields = ["*"];
+
+				var queryTask = new QueryTask(this.config.layer);
+				queryTask.execute(query, lang.hitch(this, function (resultSet) {
+						if (resultSet && resultSet.features && resultSet.features.length > 0) {
+							this._showMessage(resultSet.features.length + " feature(s) found");
+							this._drawResultsOnMap(resultSet);
+						} else {
+							this._showMessage("no feature found", "warning");
+						}
+					}), lang.hitch(this, function (err) {
+						this._showMessage(err.message, "error");
+					}));
+			},
+
+			_drawResultsOnMap : function (resultSet) {
+				this._graphicLayer.clear();
+				var resultExtent = null,
+				highlightSymbol;
+
+				switch (resultSet.geometryType) {
+				case "esriGeometryPoint":
+					highlightSymbol = new SimpleMarkerSymbol(this._symbols[resultSet.geometryType]);
+					break;
+				case "esriGeometryPolyline":
+					highlightSymbol = new SimpleLineSymbol(this._symbols[resultSet.geometryType]);
+					break;
+				case "esriGeometryPolygon":
+					highlightSymbol = new SimpleFillSymbol(this._symbols[resultSet.geometryType]);
+					break;
+				};
+
+				array.forEach(resultSet.features, lang.hitch(this, function (feature) {
+						this._graphicLayer.add(new Graphic(
+							feature.geometry,
+							highlightSymbol,
+							feature.attributes,
+							this._infoTemplate));
+
+						if (resultSet.geometryType === "esriGeometryPoint") {
+							if (resultExtent) {
+								resultExtent = resultExtent.union(new Extent(
+									feature.geometry.x, feature.geometry.y,
+									feature.geometry.x, feature.geometry.y,
+									feature.geometry.spatialReference));
+							} else {
+								resultExtent = new Extent(
+									feature.geometry.x, feature.geometry.y,
+									feature.geometry.x, feature.geometry.y,
+									feature.geometry.spatialReference);
+							}
+						} else {
+							if (resultExtent) {
+								resultExtent = resultExtent.union(feature.geometry.getExtent());
+							} else {
+								resultExtent = feature.geometry.getExtent();
+							}
+						}
+					}));
+
+				if (resultExtent) {
+					if (resultExtent.getHeight() === 0 || resultExtent.getWidth() === 0) {
+						this.map.centerAndZoom(resultExtent.getCenter(), 15);
+					} else {
+						this.map.setExtent(resultExtent, true);
+					}
+				}
+			}
+
+		});
+
+	return clazz;
+});
---
+++ widgets/SearchTwnship/nls/strings.js
@@ -0,0 +1,16 @@
+define({
+  root: ({
+    _widgetLabel: "SearchtTwnship",
+	searchTitle: "Search Township/Range",
+	searchPrompt: "Use this form to zoom the map to a PLSS section, or to a specific township-range combination. Tozoom to a township-range, do not select a sectionnumber from the \"Section\" dropdown box.",
+	stateLabel: "State", 
+	sectionLabel: "Section",
+	townshipLabel: "Township",
+	rangeLabel: "Range",
+	northDirLabel: "N",
+	southDirLabel: "S",
+	eastDirLabel: "E",
+	westDirLabel: "W",
+    endSteps: "Search"
+  })
+});
