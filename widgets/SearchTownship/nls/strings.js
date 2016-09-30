403d5e18da1d13dbd5ed948e19a34eb81528430a
jaredlang <jaredlang76@gmail.com>
2016-09-30 10:31:56 -0500

    corrected the typo in widget NLS

--- widgets/SearchTownship/nls/strings.js
+++ widgets/SearchTownship/nls/strings.js
@@ -2,7 +2,7 @@ define({
   root: ({
     _widgetLabel: "SearchtTownship",
 	searchTitle: "Search Township/Range",
-	searchPrompt: "Use this form to zoom the map to a PLSS section, or to a specific township-range combination. Tozoom to a township-range, do not select a sectionnumber from the \"Section\" dropdown box.",
+	searchPrompt: "Use this form to zoom the map to a PLSS section, or to a specific township-range combination. To zoom to a township-range, do not select a section number from the \"Section\" dropdown box.",
 	stateLabel: "State", 
 	sectionLabel: "Section",
 	townshipLabel: "Township",
