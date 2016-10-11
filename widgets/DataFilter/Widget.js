8ddb1cc457f51cad5a2b919ff05b14a06bb7eb91
jaredlang <jaredlang76@gmail.com>
2016-10-11 14:40:02 -0500

    Turned visibility on for additional layers
    along with search results, even the result is empty. At lease the query
    target layer will be turned on. However, whether they are displayed on
    the map depends on other factors such as zoom scale and scale
    dependency.

--- widgets/DataFilter/Widget.js
+++ widgets/DataFilter/Widget.js
@@ -111,6 +111,12 @@ define([
 				if (this.config.renderSymbols) {
 					this._symbols = this.config.renderSymbols; 
 				}
+				
+				if (!this.config.visibleLayers) {
+					this.config.visibleLayers = []; 
+				} 
+				// by default, display the query layer 
+				this.config.visibleLayers.push(this.config.layer); 				
 			},
 
 			_initSearchForm : function () {
@@ -319,6 +325,8 @@ define([
 							} else {
 								this._showMessage("no feature found", "warning");
 							} 
+							// turn on query layer and other relevant layers
+							this._displayMapLayers(this.config.visibleLayers); 
 						} else {
 							// in case null resultSet, set empty value
 							resultSet = {"features": []}; 
@@ -485,6 +493,31 @@ define([
 				if (attributeTableWidget) {
 					widgetManager.closeWidget(attributeTableWidget);
 				} 
+			}, 
+			
+			_displayMapLayers : function(layerUrls) {
+				if (layerUrls && layerUrls.length > 0)  {
+					array.forEach(this.map.layerIds, lang.hitch(this, function(layerId) {
+						var dynamicLayer = this.map.getLayer(layerId); 
+						var subLayersVisible = dynamicLayer.visibleLayers; 
+						var visibilityChanged = false; 
+						array.forEach(layerUrls, lang.hitch(this, function(layerUrl) {
+							if (layerUrl.indexOf(dynamicLayer.url) > -1) {
+								var layerUrlParts = layerUrl.split("/"); 
+								var subLayerIndex = layerUrlParts[layerUrlParts.length-1]; 
+								if (array.indexOf(subLayersVisible, subLayerIndex) == -1) {
+									subLayersVisible.push(Number(subLayerIndex)); 
+									visibilityChanged = true; 
+								} 
+							}
+						}));
+						if (visibilityChanged == true) {
+							subLayersVisible.sort(); 
+							dynamicLayer.setVisibleLayers(subLayersVisible); 
+							dynamicLayer.setVisibility(true); 
+						}
+					})); 
+				}
 			}
 
 		});
