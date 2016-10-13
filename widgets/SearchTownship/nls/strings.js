d35b2469034535da9acd365846afb57b7188e15d
jaredlang <jaredlang76@gmail.com>
2016-10-13 15:17:30 -0500

    changed searchPrompt for SearchTownship
    different wording

--- widgets/SearchTownship/nls/strings.js
+++ widgets/SearchTownship/nls/strings.js
@@ -2,7 +2,7 @@ define({
   root: ({
     _widgetLabel: "Searcht Township & Range",
 	searchTitle: "Search Township/Range",
-	searchPrompt: "Use this form to zoom the map to a PLSS section, or to a specific township-range combination. To zoom to a township-range, do not select a section number from the \"Section\" dropdown box.",
+	searchPrompt: "Use this form to zoom to a PLSS section, or a specific township/range combination. To zoom to a particular township/range, do not select a section number from the 'Section' dropdown box",
 	stateLabel: "State", 
 	sectionLabel: "Section",
 	townshipLabel: "Township",
