7f2239a7525402445c1058eab3c8e469fa479b75
jaredlang <jaredlang76@gmail.com>
2016-11-04 12:03:12 -0500

    allowed relative URLs in the config
    the widget investigates the map item and determines the absolute URL, so
    when the webapp is deployed for another portal there will be no need for
    URL changes. Use absolute URLs to avoid the map host change.

--- widgets/SearchData/Widget.js
+++ widgets/SearchData/Widget.js
@@ -100,6 +100,7 @@ define([
 
 			postCreate : function () {
 				this.inherited(arguments);
+				this._qualifyURLs(); 
 				this._initSearch();
 				this._initSearchForm();
 
@@ -108,6 +109,11 @@ define([
 						views : [this.optionSection, this.filterSection]
 					});
 				domConstruct.place(this.viewStack.domNode, this.ParameterSection, "only");
+			},
+			
+			_qualifyURLs : function () {
+				this.config.layer = this._convertToAbsURL(this.config.layer); 
+				this.config.visibleLayers = this._convertToAbsURLs(this.config.visibleLayers);				
 			},
 
 			_initSearch : function () {
@@ -488,6 +494,44 @@ define([
 					}
 				}
 			},
+			
+			_convertToAbsURLs : function(relativeURLs) {
+				if (relativeURLs instanceof Array) {
+					var absoluteURLs = []; 
+					for(var u=0,l=relativeURLs.length; u<l; u++) {
+						absoluteURLs[u] = this._convertToAbsURL(relativeURLs[u]); 
+					} 
+					return absoluteURLs;
+				}
+				return relativeURLs; // return as is
+			},
+			
+			_convertToAbsURL : function(relativeURL) {
+				if (relativeURL && /^https?:\/\//i.test(relativeURL) == false) {
+					var urlRegExp = new RegExp(relativeURL.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&") + "$", "i"); 
+					if (this.map.itemInfo && this.map.itemInfo.itemData && this.map.itemInfo.itemData.operationalLayers) {
+						var operationalLayers = this.map.itemInfo.itemData.operationalLayers; 
+						for(var p=0,pl=operationalLayers.length; p<pl; p++) {
+							var serviceLayer = operationalLayers[p]; 
+							for(var f=0,fl=serviceLayer.resourceInfo.layers.length; f<fl; f++) {
+								var featureLayer = serviceLayer.resourceInfo.layers[f]; 
+								var layerURL = serviceLayer["url"] + "/" + featureLayer["id"]; 
+								if (urlRegExp.test(layerURL) == true) {
+									return layerURL; 
+								}
+							}
+							for(var t=0,tl=serviceLayer.resourceInfo.tables.length; t<tl; t++) {
+								var featureTable = serviceLayer.resourceInfo.tables[t]; 
+								var tableURL = serviceLayer["url"] + "/" + featureTable["id"]; 
+								if (urlRegExp.test(tableURL) == true) {
+									return tableURL; 
+								}
+							} 
+						}
+					} 
+				}
+				return relativeURL; // return as is 
+			}, 
 
 			_isKeyPrintable : function(keyCode) {
 				return (
