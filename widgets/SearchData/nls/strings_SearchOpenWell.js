de2f472d73da532a3c10e3286734d29028505d3d
jaredlang <jaredlang76@gmail.com>
2016-11-29 10:48:50 -0600

    created widget-specific nls-string files and icons

---
+++ widgets/SearchData/nls/strings_SearchOpenWell.js
@@ -0,0 +1,10 @@
+define({
+  root: ({
+    _widgetLabel: "Search Open Wells",
+    limitToMapExtent: "Limit to Current Map Extent",
+    cancelSteps: "Cancel",
+    nextStep: "Next",
+    prevStep: "Back",
+    endSteps: "Search"
+  })
+});
